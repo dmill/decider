bd4a2ab9a5df8e56c0b02988aa65948d08c178a4
Douglas Mill <dmill989@gmail.com>
2013-10-12 13:22:56 -0700

    generate rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Decider::Application.config.secret_key_base = '20a5342d0138c2e2f3cfcd7d2bc00a3c4f4bc55bd180b0cb7867331f40a17c4e1f94a02d2d166f2d44583876d101b6bbdd0f9f045d2f06513f619f52b08da34f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Decider::Application.config.session_store :cookie_store, key: '_decider_session'
