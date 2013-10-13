d5c79c9e624c941fac0db26d12a4d4aee28ef529
Douglas Mill <dmill989@gmail.com>
2013-10-13 11:15:59 -0700

    index page lists links to all problems

--- spec/features/problems_index_spec.rb
+++ spec/features/problems_index_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 feature "index page" do
 	scenario "user clicks a problem link" do
+		FactoryGirl.create(:problem)
+		test_problem = Problem.first
 		visit root_path
-		click_link 'problem_5'
-		expect(current_path).to eq(new_question_path)
+		click_link "#{test_problem.text}"
+		expect(current_path).to eq(problem_path(Problem.first))
 	end
 	scenario "user clicks New Problem button"
 end
