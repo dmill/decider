839bf48fb4828f88f0aaef965468b6494fcee7a7
Douglas Mill <dmill989@gmail.com>
2013-10-13 11:25:19 -0700

    new problem button on problems index page

--- spec/features/problems_index_spec.rb
+++ spec/features/problems_index_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 feature "index page" do
+	before(:each) do
+		@test_problem = FactoryGirl.create(:problem)
+	end
 	scenario "user clicks a problem link" do
-		FactoryGirl.create(:problem)
-		test_problem = Problem.first
 		visit root_path
-		click_link "#{test_problem.text}"
+		click_link "#{@test_problem.text}"
 		expect(current_path).to eq(problem_path(Problem.first))
 	end
-	scenario "user clicks New Problem button"
+	scenario "user clicks New Problem button" do
+		visit root_path
+		click_link "New Problem"
+		expect(current_path).to eq(new_problem_path)
+	end
 end
